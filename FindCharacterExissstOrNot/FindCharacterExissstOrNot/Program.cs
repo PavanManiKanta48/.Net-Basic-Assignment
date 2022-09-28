64f20e07c4995d2c0b24fe57315cb2bc30b31486
PavanManiKanta48 <113531282+PavanManiKanta48@users.noreply.github.com>
2022-09-28 10:02:29 +0530

    Add files via upload

---
+++ FindCharacterExissstOrNot/FindCharacterExissstOrNot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindCharacterExissstOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a string : ");
+            string name=Console.ReadLine();
+            Console.Write("enter a 1 Character : ");
+            char character = char.Parse(Console.ReadLine());
+            Boolean result=name.Contains(character);
+            Console.WriteLine($"Does string contain specified character? {result}");
+
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Numbers From 1 To 100 :");
+            for(int i=1;i<100;i++)
+            {
+               int count = 0;
+                if (i >= 1)
+                {
+                    for(int j=2;j<i;j++)
+                    {
+                        if(i%j == 0)
+                            count=1;
+                        break;
+
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i+ " ");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
